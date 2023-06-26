802020b2bb3614883352a3a01e230b03744d44cb
ck <erncy07@gmail.com>
2023-06-26 20:28:53 +0800

    first

---
+++ fish/functions/fish_prompt.fish
@@ -0,0 +1,1 @@
+/home/ck/.local/share/omf/themes/one/fish_prompt.fish
---
+++ hypr/scripts/wp-change.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+feh --recursive --randomize --bg-fill ~/picture/wallpaper
+#feh --recursive --randomize --bg-fill ~/Pictures/wallpapers/view
